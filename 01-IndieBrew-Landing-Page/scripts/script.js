3cbd4671426cf90be9172f22836f710a07abe53d
Spirit <stackritesh@gmail.com>
2024-03-11 02:34:58 +0530

    modify nav querySelector

--- 01-IndieBrew-Landing-Page/scripts/script.js
+++ 01-IndieBrew-Landing-Page/scripts/script.js
@@ -1,6 +1,6 @@
 window.onload = function () {
     const menu_btn = document.querySelector('.hamburger');
-    const nav = document.querySelector('.secondary-nav');
+    const nav = document.querySelector('.navbar');
     menu_btn.addEventListener('click', function () {
         menu_btn.classList.toggle('is-active');
         nav.classList.toggle('is-active');
